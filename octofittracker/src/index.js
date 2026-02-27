3b01914399e3e8b4be060bac2a2ffd4efbeb3d11
mtec1091 <michael.tecmire@accenture.com>
2026-02-27 21:46:03 +0000

    push 7

--- octofittracker/src/index.js
+++ octofittracker/src/index.js
@@ -1,9 +1,18 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Set REACT_APP_CODESPACE_NAME from window.location if not set
+if (!process.env.REACT_APP_CODESPACE_NAME) {
+  const host = window.location.host;
+  const codespace = host.split('-8000')[0];
+  process.env.REACT_APP_CODESPACE_NAME = codespace;
+  console.log('Detected Codespace Name:', codespace);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +21,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
